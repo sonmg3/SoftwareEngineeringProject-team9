50873b22cedfe575c1ba2221020a102e060d6e52
JHKim <fnrldk98@naver.com>
2015-11-20 21:17:53 +0900

    디비 테이블 수정
    디비 유저테이블 수정

--- src/SignInProcess.java
+++ src/SignInProcess.java
@@ -177,7 +177,7 @@ public class SignInProcess extends JFrame implements ActionListener{
 	public boolean dupliChkButton(){		
 		
 		try {
-			query = con.prepareStatement("select TeamName from UserInfo where TeamName = ?");
+			query = con.prepareStatement("select TeamName from User where TeamName = ?");
 			query.setString(1, inputTeamName);
 			rset = query.executeQuery();		
 			rset.next();
@@ -210,7 +210,7 @@ public class SignInProcess extends JFrame implements ActionListener{
         if (result == JOptionPane.YES_OPTION) {    	
        	
        	try {
-       		String query1 = "INSERT INTO UserInfo(TeamName,Password,PhoneNumber,Email)";
+       		String query1 = "INSERT INTO User(TeamName,Password,PhoneNumber,Email)";
        		query1 += "VALUES (?, ?, ?, ?)";
 				query = con.prepareStatement(query1);
 				query.setString(1, inputTeamName);	
